fb70dc982f80a27ec90a31760b0165ec2df07956
miguel-acevedo <miguel.acevedo@state.ca.gov>
2017-07-07 13:40:44 -0700

    Updated

--- mainCrawl.py
+++ mainCrawl.py
@@ -9,6 +9,8 @@ from bs4 import BeautifulSoup
 checkedLinks = list()
 external = list()
 
+base = "http://www.cphcs.ca.gov/"
+
 def checkWebsite(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -17,25 +19,36 @@ def checkWebsite(url):
         return 0
         print("None url: " , url)
     for a in soup.find_all('a', href=True):
+        #print(a['href'])
         if (a['href'].startswith("#")) or (a['href'] == '/') or (a['href'].startswith("mailto")) or (a['href'] in checkedLinks):
             continue
 
         if ("http" in a['href']): #Checks if external link
             if url.split("//",1)[1] not in a['href']:
-                external.append(a['href'])
-                continue
+                if not "www.cphcs.ca.gov" in a['href']:
+                    external.append(a['href'])
+                    #print(a['href'])
+                    continue
 
         checkedLinks.append(a['href'])
         if "www." not in a['href']:
             #print("www on ", a['href'])
-            a['href'] = "http://www.cphcs.ca.gov/" + a['href']
+            if url == base:
+                a['href'] = base + a['href']
+            else:
+                if (a['href'].startswith("/")):
+                    a['href'] = a['href'][1:]
+                print("Url: " , url ,"Original: " , a['href'])
+                a['href'] = url.rsplit('/',1)[0] + "/" + a['href']
 
 
         if not (a['href'].startswith("http")):
             a['href'] = "http://" + a['href']
 
-        if "../" in a['href']:
-            a['href'] = "http://www.cphcs.ca.gov/pra.aspx"
+
+        #if "../" in a['href']:
+            #a['href'] = "http://www.cphcs.ca.gov/pra.aspx"
+
 
         print("Found the URL:", a['href'])
         if not (a['href'].endswith(".pdf") or a['href'].endswith(".xls") or a['href'].endswith(".docx")):
